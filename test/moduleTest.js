cbd5eb0c3be35bee0f44533bcf2f50516fca04a4
mwiesmueller <martin.wiesmueller@werbas-ag.com>
2017-03-15 10:30:35 +0100

    Expand tests set coverdata in gitignore

--- test/moduleTest.js
+++ test/moduleTest.js
@@ -10,9 +10,51 @@ describe('Testing Eventstoremodule....', () => {
     es = eventstore.start();
   });
 
+  describe('.... subfunction start ....', () => {
+    it('... is of type function', (done) => {
+      assert.that(es.start).is.ofType('function');
+      done();
+    });
+  });
+
   describe('.... subfunction saveEvent ....', () => {
     it('... is of type function', (done) => {
       assert.that(es.saveEvent).is.ofType('function');
+      done();
+    });
+  });
+
+  describe('.... subfunction createGlobalSnapshot ....', () => {
+    it('... is of type function', (done) => {
+      assert.that(es.createGlobalSnapshot).is.ofType('function');
+      done();
+    });
+  });
+
+  describe('.... subfunction createAggregateSnapshot ....', () => {
+    it('... is of type function', (done) => {
+      assert.that(es.createAggregateSnapshot).is.ofType('function');
+      done();
+    });
+  });
+
+  describe('.... subfunction getAggregateInformationbyRange ....', () => {
+    it('... is of type function', (done) => {
+      assert.that(es.getAggregateInformationbyRange).is.ofType('function');
+      done();
+    });
+  });
+
+  describe('.... subfunction getLastAggregateInformation ....', () => {
+    it('... is of type function', (done) => {
+      assert.that(es.getLastAggregateInformation).is.ofType('function');
+      done();
+    });
+  });
+
+  describe('.... subfunction getCompleteAggregateInformation ....', () => {
+    it('... is of type function', (done) => {
+      assert.that(es.getCompleteAggregateInformation).is.ofType('function');
       done();
     });
   });
