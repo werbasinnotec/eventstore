25ec05ab808fc8f1146e803c8b554dc70a6d6185
mwiesmueller <martin.wiesmueller@werbas-ag.com>
2017-03-16 16:01:13 +0100

    Implementate function to create an eventObject

---
+++ test/events/createEventObjectTest.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const assert = require('assertthat');
+const createEventObject = require('../../lib/events/createEventObject');
+
+describe('createEventObject...', () => {
+  it('... is of type function', (done) => {
+    assert.that(createEventObject).is.ofType('function');
+    done();
+  });
+
+  it('... callbacks an error when obj is not defined', (done) => {
+    createEventObject(undefined, (err) => {
+      assert.that(err).is.equalTo('Function is called without data-object');
+      done();
+    });
+  });
+
+  it('... callbacks an error when no aggreagateID is defined in the object', (done) => {
+    createEventObject({}, (err) => {
+      assert.that(err).is.equalTo('Function is called without aggreagateID');
+      done();
+    });
+  });
+
+  it('... callbacks an error when no aggreagateID is defined in the object', (done) => {
+    createEventObject({ aggregateID: 'ABC' }, (err) => {
+      assert.that(err).is.equalTo('Function is called without aggregate');
+      done();
+    });
+  });
+
+  it('... callbacks an error when no payload is defined in the object', (done) => {
+    createEventObject({ aggregateID: 'ABC', aggregate: 'billing' }, (err) => {
+      assert.that(err).is.equalTo('Function is called without payload');
+      done();
+    });
+  });
+
+  it('... callbacks an object when process is done', (done) => {
+    createEventObject({ aggregateID: 'ABC', aggregate: 'billing', payload: { foo: 'bar' }}, (err, eventObj) => {
+      if (err) {
+        throw err;
+      }
+
+      assert.that(eventObj).is.ofType('object');
+      done();
+    });
+  });
+});
